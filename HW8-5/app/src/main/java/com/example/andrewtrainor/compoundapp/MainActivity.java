c9df2258d876d95afb013d6ab1c7e76b83ce63ff
Andrew Trainor <atrainor2019@fau.edu>
2020-11-01 16:06:54 -0500

    Updated NavBar and added drawable UI elements

--- HW8-5/app/src/main/java/com/example/andrewtrainor/compoundapp/MainActivity.java
+++ HW8-5/app/src/main/java/com/example/andrewtrainor/compoundapp/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.andrewtrainor.compoundapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String USER_INPUT = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,20 +25,30 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_reco_weather:
-                        Toast.makeText(MainActivity.this, "Speech Recognition Weather", Toast.LENGTH_SHORT).show();
-                        break;
+
                     case R.id.action_weather_results:
-                        Toast.makeText(MainActivity.this, "Weather Results", Toast.LENGTH_SHORT).show();
-                        break;
+
                     case R.id.action_weather_map:
-                        Toast.makeText(MainActivity.this, "Weather Map", Toast.LENGTH_SHORT).show();
-                        break;
+
                     case R.id.action_weather_history:
-                        Toast.makeText(MainActivity.this, "Weather History", Toast.LENGTH_SHORT).show();
-                        break;
                 }
                 return true;
             }
         });
+    }
+
+    //called when the user taps the search button and sends the info to the second activity
+    public void sendWeatherInfo(View view){
+        Intent intent = new Intent(this, SecondaryActivity.class);
+        EditText editText = (EditText) findViewById(R.id.weatherInput);
+
+        //get the userinput
+        String userInput = editText.getText().toString();
+
+        //pass the userinput data to the next activity
+        intent.putExtra(USER_INPUT, userInput);
+
+        //move to the next activity
+        startActivity(intent);
     }
 }
